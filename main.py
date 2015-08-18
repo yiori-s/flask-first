fb8767be0d71f405d5902363920482e92e1d4d06
Iori SUGIYAMA <amedama.ageru.is@gmail.com>
2015-08-18 12:02:46 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+	if request.method=='POST':
+		return render_template('index.html',title="Flask test POST", body=request.form['message'], nums=[3,4,5], dict={'a':1, 'b':2})
+	else:
+		return render_template('index.html',title="Flask test GET", body="Welcome", nums=[0,1,2], dict={'a':3, 'b':4})
+
+if __name__ == "__main__":
+	app.run(debug=True)
